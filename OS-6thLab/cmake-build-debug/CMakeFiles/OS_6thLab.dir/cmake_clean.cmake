460edfb11db9445fee61b77167f0e43047c7b493
setihex <routerhex@qq.com>
2018-11-26 11:56:16 +0800

    It's windows version Clion 6th lab.

---
+++ OS-6thLab/cmake-build-debug/CMakeFiles/OS_6thLab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS_6thLab.dir/main.c.obj"
+  "OS_6thLab.pdb"
+  "OS_6thLab.exe"
+  "OS_6thLab.exe.manifest"
+  "libOS_6thLab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OS_6thLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ OS-6thLab/src/6thcodePt1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <malloc.h>
+
+//
+// Created by Jalr on 2018/11/26.
+//
+
+/**
+ * 位示图结构，m 行 n列的数组来表示位示图
+ */
+typedef struct {
+    int **map;              //这里直接用整型来表示位示图的状态了
+}*BitMap, _BitMap;
+
+//为 bitmap分配 m行，n列的大小
+void initMap(BitMap *bitmap, int m, int n){
+
+    *bitmap = (BitMap) malloc(sizeof(_BitMap));//首先为位示图分配内存
+    (*bitmap) -> map = ( int** ) malloc ( sizeof( int* ) * m ); //分配一个 m 行的数组
+    for(int i = 0; i < m; i++){
+        (*bitmap) -> map[i] = ( int* ) malloc ( sizeof ( int ) * n);   //分配 m 个 n 列的数组
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            (*bitmap) -> map[i][j] = 6;
+        }
+    }
+
+}
+
+//释放 bitmap 内存，需要行数即可，释放每一行的头指针
+void freeMap(BitMap bitMap, int m){
+    for(int i = 0; i < m; i++){
+        free(bitMap -> map[i]);    //释放行指针
+    }
+    free(bitMap -> map);           //释放列指针
+}
+
+void travelBitMap(BitMap bitMap,int m, int n){
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            printf("%d   \t", bitMap -> map[i][j]);
+        }
+    }
+}
+
+void main(){
+    BitMap m1;
+    int n, m = 0;
+    scanf("%d %d", &n, &m);
+    initMap(&m1, n, m);
+    travelBitMap(m1, n, m);
+}
